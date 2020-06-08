633522b9976fe78fed6e164a6d57f6f8219d0eb0
Maksim Maltcev <maximus3506@mail.ru>
2020-06-08 22:33:45 +0300

    lesson 3 hard done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-let num = 266219;
+let lang = prompt ('Введите язык в формате ru - en');
 
-let result = num.toString().split("").reduce(function(prev, current){
-    return parseInt(prev) * parseInt(current)
-});
-console.log(result);
+if (lang == 'ru'){
+    console.log ('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
+}else if (lang === 'en'){
+    console.log ('Mon, Tue, Wed, Thu< Fri, Sat, Sun')
+}else{
+    console.log ('вы ввели язык в другом формате, перезагрузите страницу и попробуйте снова');
+}
+
+switch (lang){
+    case 'ru':
+        console.log ('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
+        break;
+    case 'en':
+        console.log ('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
+        break;
+    default:
+        console.log ('вы ввели язык в другом формате, перезагрузите страницу и попробуйте снова');
+}
 
 
-total = result ** 3;
-console.log(total.toString().substr(0, 2));
+let lang_array = [];
+lang_array['ru'] = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'];
+lang_array['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+console.log(lang_array[lang]);
+
+
+
+let namePerson = prompt('Введите ваше имя');
+
+let person = (namePerson == 'Артем') ? 'директор' : (namePerson == 'Максим') ? 'преподаватель' : 'студент';
+
+console.log (person);
