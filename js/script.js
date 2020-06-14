7fe0044a72a953f5be31965bef365f29b2f77777
Maksim Maltcev <maximus3506@mail.ru>
2020-06-14 20:48:47 +0300

    не сделал последнее задание на ХАРД, тут нужно посовещаться

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-let arr = ['22', '37', '455', '104', '47', '78', '377']; 
+let week = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'],
+    weekDay = getWeekDay();
+   
+for (var key in week) {
+    if (key <5) {
+        document.write(week[key] + '<br>');
+    }
+    else {
+        document.write('<i style="font-style: italic">' + week[key] + '<br>')
+    }
+}    
 
-arr.forEach((item) => {
-  if (item.startsWith('2') || item.startsWith('4')) {
-    console.log(item);
-  }
-});
 
 
+function getWeekDay(date) {
+    date = new Date();
+    let days = week;
+    let day = date.getDay();
 
-function myFunction(n) {
-    divider:
-        for (let i = 2; i <= 100; i++) {
-            for (let j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) continue divider;     
-            }
-            console.log(i + ' Делители этого числа:  1 и ' + i);
-        }
-}
-myFunction();
-
+    return days[day];
+};
