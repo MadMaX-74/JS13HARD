a812b4771df12bfed6d5cc30bf5cd904f976ced4
Maksim Maltcev <maximus3506@mail.ru>
2020-06-14 21:11:51 +0300

    4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 let week = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'];
-    
+
+
+function getWeekDay(date){
+    let days = week;
+  
+    return days[date.getDay()];
+  }
+  
+let date = new Date();
+let weekDay = getWeekDay(date);
+console.log(weekDay);    
    
 for (var key in week) {
     if (key <5) {
@@ -11,11 +21,3 @@ for (var key in week) {
 }    
 
 
-function getWeekDay(date) {
-    let days = week;
-  
-    return days[date.getDay()];
-  }
-  
-  let date = new Date();
-  document.write (getWeekDay(date));
